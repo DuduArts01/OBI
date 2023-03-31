c62bdffc3f902c3747a76497dbcf5984cdfb016f
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-03-31 00:21:56 +0000

    Add files via upload

---
+++ Level_2/Phase_1/2021/Camilla's_age.py
@@ -0,0 +1,10 @@
+ages = []
+
+for i in range(0, 3):
+    n = int(input())
+    ages.append(n)
+
+ages.remove(max(ages)) # remove the last age = Cibele
+ages.remove(min(ages)) # remove the youngest girl = Celeste
+
+print(ages) # Camilla's age 
