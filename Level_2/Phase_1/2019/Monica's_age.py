ca049d49b082714c3ebee9584ff933a7daa80059
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-05-29 12:16:27 -0300

    Monica's_age.py

---
+++ Level_2/Phase_1/2019/Monica's_age.py
@@ -0,0 +1,11 @@
+M = int(input())
+# age Monica
+
+A = int(input())
+# age son 1
+
+B = int(input())
+#age son 2
+
+print(max(A, B, (M - (A + B))))
+#find out the age of the third child and the older son age 
